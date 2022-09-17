b784a633d5f2d18dec0f8f77caefeaf74ddd96e3
KevinAlvss <kevin.bernardo64@gmail.com>
2022-09-17 15:55:00 -0300

    adding user name text

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,14 +3,23 @@ import "./styles.scss";
 
 export function Dashboard() {
   return (
-    <div className="dashboard-header">
-      <div>
-        <h1>
-          Our <span>Bank</span>
-        </h1>
+    <div className="container">
+      <div className="dashboard-header">
+        <div>
+          <h1>
+            Our <span>Bank</span>
+          </h1>
 
-        <Link to="/edit-account">Minha conta</Link>
+          <Link to="/edit-account">Minha conta</Link>
+        </div>
       </div>
+
+      <div className="user-name">
+        <div>
+          <h2>Olá, grupo 1</h2>
+        </div>
+      </div>
+      <div className="content"></div>
     </div>
   );
 }
