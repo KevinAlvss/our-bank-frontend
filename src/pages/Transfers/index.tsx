130c68bdbcba6ccfec47ec4f7fea8b8d0d11a683
KevinAlvss <kevin.bernardo64@gmail.com>
2022-11-16 21:46:12 -0300

    feat: estilizando transferencias

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
+import editPen from "../../images/editPen.svg";
+import deleteTrash from "../../images/deleteTrash.svg";
 
 export function Transfers() {
   return (
@@ -11,7 +13,32 @@ export function Transfers() {
       </div>
       <div className={styles.content}>
         <h1 className={styles.title}>Minhas transferências</h1>
-        <table></table>
+        <table>
+          <thead>
+            <tr>
+              <th>Destinatário</th>
+              <th>Descrição</th>
+              <th>Data</th>
+              <th>Valor</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Fulano da Silva </td>
+              <td>Blá bla bla</td>
+              <td>17/11/2022</td>
+              <td>R$ 18,90</td>
+              <td>
+                <img src={editPen} alt="editar" />
+              </td>
+              <td>
+                <img src={deleteTrash} alt="deletar" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <Link to={"/dashboard"} className={styles.voltar}>
           Voltar
         </Link>
